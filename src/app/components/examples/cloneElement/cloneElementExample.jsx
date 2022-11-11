d8df22ad3147e18270076d610369b4b54a607711
potapag <potapag@gmail.com>
2022-11-11 13:18:37 +0300

    React.cloneElement

--- src/app/components/examples/cloneElement/cloneElementExample.jsx
+++ src/app/components/examples/cloneElement/cloneElementExample.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import CardWrapper from "../../common/Card";
+import TextField from "../../common/form/textField";
 
 import SmallTitle from "../../common/typografy/smallTitle";
 const CloneElementExample = () => {
+    const field = <TextField label="email" name="email" />;
+    const handleChenge = (target) => {
+        console.log("chenge: ", target);
+    };
     return (
         <CardWrapper>
             <SmallTitle>Пример</SmallTitle>
+            {field}
+            {React.cloneElement(field, {
+                onChange: handleChenge,
+                label: "Clone email"
+            })}
         </CardWrapper>
     );
 };
